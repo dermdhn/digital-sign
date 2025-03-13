4b986ad3ea7a84f1d87b42ec47635ed2ce6b3867
Dewa Ramadhan <drmdhn33@gmail.com>
2025-03-13 13:03:13 +0700

    commit pertama

---
+++ resources/views/landscape.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="id">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Rektorat UNNES</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-gray-100 h-screen flex items-center justify-center">
+    <div class="w-full h-full bg-white shadow-lg flex flex-col">
+        <!-- Judul -->
+        <div class="bg-gray-300 text-center text-xl font-bold py-9">Judul</div>
+
+        <!-- Informasi Lantai & Jadwal -->
+        <div class="grid grid-cols-2 flex-grow">
+            <div class="bg-gray-500 text-white text-center flex items-center justify-center text-lg">Informasi Lantai
+            </div>
+            <div class="bg-gray-400 text-white text-center flex items-center justify-center text-lg">Jadwal & Agenda
+            </div>
+        </div>
+
+        <div class="bg-gray-900 py-8 flex-shrink-0">
+            <div class="grid grid-cols-5 gap-5 px-5">
+                <!-- Kotak Kehadiran -->
+                <div class="bg-gray-400 py-20 rounded-xl flex items-center justify-center relative">
+                    <div
+                        class="bg-gray-700 text-white text-center py-2 px-10 rounded-t-lg absolute bottom-0 left-1/2 transform -translate-x-1/2">
+                        Hadir
+                    </div>
+                </div>
+                <div class="bg-gray-400 py-20 rounded-xl flex items-center justify-center relative">
+                    <div
+                        class="bg-gray-700 text-white text-center py-2 px-10 rounded-t-lg absolute bottom-0 left-1/2 transform -translate-x-1/2">
+                        Hadir
+                    </div>
+                </div>
+                <div class="bg-gray-400 py-20 rounded-xl flex items-center justify-center relative">
+                    <div
+                        class="bg-gray-700 text-white text-center py-2 px-10 rounded-t-lg absolute bottom-0 left-1/2 transform -translate-x-1/2">
+                        Hadir
+                    </div>
+                </div>
+                <div class="bg-gray-400 py-20 rounded-xl flex items-center justify-center relative">
+                    <div
+                        class="bg-gray-700 text-white text-center py-2 px-10 rounded-t-lg absolute bottom-0 left-1/2 transform -translate-x-1/2">
+                        Hadir
+                    </div>
+                </div>
+                <div class="bg-gray-400 py-20 rounded-xl flex items-center justify-center relative">
+                    <div
+                        class="bg-gray-700 text-white text-center py-2 px-10 rounded-t-lg absolute bottom-0 left-1/2 transform -translate-x-1/2">
+                        Hadir
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <!-- More Information -->
+        <div class="bg-white text-center text-lg font-semibold py-4 border-t flex-shrink-0">More information</div>
+    </div>
+</body>
+
+</html>
