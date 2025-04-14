e982a825c6ec6bc32ab64c4e390dfa4bec7e64da
dermdhn <drmdhn33@gmail.com>
2025-04-14 14:43:12 +0700

    landscape done ketoe

--- resources/views/partials/floor-content-4.blade.php
+++ resources/views/partials/floor-content-4.blade.php
@@ -3,7 +3,7 @@
     <div class="text-gray-700 font-poppins transition-all duration-300">
         <h3
             class="font-bold text-5xl mb-4 text-gray-800 text-center transform transition-all duration-300 hover:translate-y-[-2px]">
-            <i class="fas fa-door-open mr-2"></i>Informasi Ruangan
+            Informasi Ruangan
         </h3>
         <div class="w-full h-1 bg-gray-200 mb-4 rounded-full transform transition-all duration-300"></div>
         <ul
